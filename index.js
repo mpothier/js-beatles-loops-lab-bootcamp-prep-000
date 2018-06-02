0e47d6f889a4e291945e406a94807f5934038b25
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-02 23:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var myArray = []
-  for (var i = 0; i < musicians.length; )
+  for (var i = 0; i < musicians.length; i++) {
+    myArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return myArray
 }
