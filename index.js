709bf00c4935229598302bc47fadd3f773597212
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-02 23:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
+  var newArray = []
   let i = 0
   while (i < facts.length) {
     newArray = facts[i] += "!!!"
